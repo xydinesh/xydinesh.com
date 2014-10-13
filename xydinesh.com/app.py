b9cb21cb48b0248cba5eac0fb8a59340052752a0
Dinesh Weerapurage <xydinesh@gmail.com>
2014-10-13 18:12:19 -0400

    adding Frozen-Flask

--- xydinesh.com/app.py
+++ xydinesh.com/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 
 # If you get an error on the next line on Python 3.4.0, change to: Flask('app')
@@ -7,6 +8,8 @@ from flask_flatpages import FlatPages
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 pages = FlatPages(app)
+freezer = Freezer(app)
+
 from routes import *
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -14,6 +17,12 @@ wsgi_app = app
 
 if __name__ == '__main__':
     import os
+    import sys
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        freezer.freeze()
+        sys.exit(0)
+
     host = os.environ.get('SERVER_HOST', 'localhost')
     try:
         port = int(os.environ.get('SERVER_PORT', '5555'))
