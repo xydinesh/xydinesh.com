3bc2efff2d6ade30084e13ee215ea0009045794f
Dinesh Weerapurage <xydinesh@gmail.com>
2015-01-02 11:33:42 -0500

    remove freezer from  website

--- xydineshcom/__init__.py
+++ xydineshcom/__init__.py
@@ -4,13 +4,11 @@ The flask application package.
 
 from flask import Flask, url_for, render_template
 from flask_flatpages import FlatPages
-from flask_frozen import Freezer
 
 # If you get an error on the next line on Python 3.4.0, change to: Flask('app')
 # where app matches the name of this file without the .py extension.
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 pages = FlatPages(app)
-freezer = Freezer(app)
 
 import xydineshcom.views
